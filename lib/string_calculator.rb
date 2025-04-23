eb61ee413f33d00d7e4178b47293003e1737eb68
kirti09 <kirtitrikha09@gmail.com>
2025-04-23 22:29:19 +0530

    STEP5: Handles custom deliiters

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,7 +3,15 @@ class StringCalculator
   # just to invoke it directly(without creating instance) creating a class method
   def self.add(numbers)
     return 0 if numbers.empty?
-    # handles new line between the numbers
-    numbers.split(/[\n,]/).map(&:to_i).sum
+  
+    delimiter = /[\n,]/
+    # As numbers is string so here are the checks
+    if numbers.start_with?("//")
+      custom_delimiter = numbers.match(%r{//(.)\n})[1]
+      numbers = numbers.split("\n", 2).last
+      delimiter = /[#{Regexp.escape(custom_delimiter)}\n]/
+    end
+  
+    numbers.split(delimiter).map(&:to_i).sum
   end
 end
