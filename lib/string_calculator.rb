442b6b96315a6b6b4e697a33a26ef827aa4c2842
kirti09 <kirtitrikha09@gmail.com>
2025-04-23 20:32:28 +0530

    STEP4: Handles newline between the numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,7 +3,7 @@ class StringCalculator
   # just to invoke it directly(without creating instance) creating a class method
   def self.add(numbers)
     return 0 if numbers.empty?
-    # handle any amount of comma separated numbers
-    numbers.split(",").map(&:to_i).sum
+    # handles new line between the numbers
+    numbers.split(/[\n,]/).map(&:to_i).sum
   end
 end
